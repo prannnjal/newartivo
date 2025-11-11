af57c89a02ef8f30d2ad4ad9bebf60a7ca83166d
prannnjal_ <pranjalkumar445@gmail.com>
2025-11-11 18:45:47 +0530

    37

--- src/app/case-studies/page.js
+++ src/app/case-studies/page.js
@@ -1,23 +1,6 @@
-import React from "react";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
-import { DesignSection } from "@/components/DesignSection";
-import { OurWorkSection } from "@/components/OurWorkSection";
-import { TestimonialsSection } from "@/components/TestimonialsSection";
-import { ContactSection } from "@/components/ContactSection";
+import { redirect } from "next/navigation";
 
 export default function CaseStudies() {
-  return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-      <main className="pt-24">
-        <DesignSection />
-        <OurWorkSection />
-        <TestimonialsSection />
-        <ContactSection />
-      </main>
-      <Footer />
-    </div>
-  );
+  redirect("/gp-hybrid-bike");
 }
 
